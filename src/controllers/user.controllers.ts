cfdb8cfaf2cd4efebbbc245cd119bd166d7ee0de
Jorge Sader <Jorge.Sader@gmail.com>
2023-08-15 22:19:31 -0400

    refactor config file

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,15 +1,16 @@
-import { User } from "../models/user.model.js";
+import { Request, Response, NextFunction } from "express";
+import User from "../models/user.model.js";
 
-export const getUserList = async (req: any, res: any, next: any) => {
+export const getUserList = async (req: Request, res: Response, next: NextFunction) => {
   const userList = await User.find();
   console.log("🚀 ~ file: user.controllers.ts:5 ~ getUserList ~ userList:", userList);
 
   return res.status(211).send(userList);
 };
 
-export const getUser = async (req: any, res: any, next: any) => {
+export const getUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const user = await User.findById(req.user.id);
+    const user = await User.findById(req.user!.id);
     if (!user) {
       req.flash('error', 'Invalid ID!');
       res.status(404);
